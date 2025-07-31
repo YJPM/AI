7d93146c8dcc3ff5ba1b951a8da00fc081af86cf
licun <18860900110@163.com>
2025-07-31 11:58:10 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,7 @@ module.exports = (env, argv) => {
     entry: './src/index.js',
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'index.js',
-      library: {
-        type: 'window',
-        name: 'AIAssistantExtension'
-      },
-      globalObject: 'window'
+      filename: 'index.js'
     },
     module: {
       rules: [
